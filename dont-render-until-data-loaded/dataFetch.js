0c58f14960f859c29022ed5c6fa59d7e5adb5fec
romgerman <romgerman@users.noreply.github.com>
2019-07-16 22:21:37 +0300

    update

--- dont-render-until-data-loaded/dataFetch.js
+++ dont-render-until-data-loaded/dataFetch.js
@@ -1,21 +1,23 @@
-var mixin = {
+let mixin = {
   created() {
-    let hook = this.$options.dataFetch;
-    let render = this.$options.render.bind(this);
-    let isRendering = false;
+    let hook = this.$options.dataFetch
+    let render = this.$options.render.bind(this)
+    let isRendering = false
 
     this.$options.render = function(createElement, context) {
-      if (isRendering) return render(createElement, context);
-      else return null;
-    };
+      return isRendering ? render(createElement, context) : null
+    }
 
     if (typeof hook === "function") {
       hook.call(this).then(() => {
-        isRendering = true;
-        this.$forceUpdate();
-      });
+        isRendering = true
+        this.$forceUpdate()
+      })
+    } else {
+      isRendering = true
+      this.$forceUpdate()
     }
   }
-};
+}
 
-export default mixin;
+export default mixin
